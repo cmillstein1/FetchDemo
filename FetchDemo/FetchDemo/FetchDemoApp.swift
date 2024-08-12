c749be40fdeeb43928ae1d9920ad50da5c75628b
cmillstein1 <cmillstein1@me.com>
2024-08-12 14:15:17 -0400

    Add files via upload

---
+++ FetchDemo/FetchDemo/FetchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDemoApp.swift
+//  FetchDemo
+//
+//  Created by Casey Millstein New on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
