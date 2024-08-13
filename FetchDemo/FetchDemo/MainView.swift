b9c4b367dddf0d0f89f1bd4baa83110c1e270d06
Casey Millstein <cmillstein1@me.com>
2024-08-12 23:26:57 -0400

    Enhancements Continued + app icon

--- FetchDemo/FetchDemo/MainView.swift
+++ FetchDemo/FetchDemo/MainView.swift
@@ -9,69 +9,82 @@ import SwiftUI
 
 struct MainView: View {
     @State private var meals: [Meal] = []
+    @State private var isLoading = true
     private let apiService = APIService()
     
     var body: some View {
         NavigationView {
-            List(meals.sorted {
-                $0.strMeal < $1.strMeal
-            }) { meal in
-                ZStack {
-                    NavigationLink(destination: MealDetailView(mealId: meal.idMeal)) {
-                        EmptyView()
-                    }
-                    .opacity(0)
-                    
-                    HStack {
-                        AsyncImage(url: URL(string: meal.strMealThumb)) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 60, height: 60)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .shadow(radius: 5)
-                        } placeholder: {
-                            ProgressView()
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.purple.opacity(0.5)]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                if isLoading {
+                    ProgressView("Loading Meals...")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .scaleEffect(1.5)
+                } else {
+                    List(meals.sorted {
+                        $0.strMeal < $1.strMeal
+                    }) { meal in
+                        ZStack {
+                            NavigationLink(destination: MealDetailView(mealId: meal.idMeal)) {
+                                EmptyView()
+                            }
+                            .opacity(0)
+                            
+                            HStack {
+                                AsyncImage(url: URL(string: meal.strMealThumb)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 60, height: 60)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .shadow(radius: 5)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text(meal.strMeal)
+                                        .font(.headline)
+                                        .foregroundColor(.primary)
+                                }
+                                .padding(.leading, 10)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.purple)
+                            }
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color(UIColor.systemBackground))
+                                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                            )
                         }
-                        
-                        VStack(alignment: .leading) {
-                            Text(meal.strMeal)
-                                .font(.headline)
-                                .foregroundColor(.secondary)
-                        }
-                        .padding(.leading, 10)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.purple)
+                        .listRowSeparator(.hidden)
+                        .listRowBackground(Color.clear)
                     }
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(Color(UIColor.systemBackground))
-                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
-                    )
+                    .listStyle(PlainListStyle())
+                    .navigationTitle("Desserts")
                 }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color.clear)
             }
-            .listStyle(PlainListStyle())
-            .navigationTitle("Desserts")
-            .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
             .task {
                 do {
                     meals = try await apiService.fetchDesserts()
+                    isLoading = false
                 } catch {
                     print("Error fetching meals: \(error.localizedDescription)")
+                    isLoading = false
                 }
             }
         }
-        .accentColor(.purple)
+        .accentColor(.black)
     }
     
 }
 
-#Preview {
-    MainView()
-}
+    #Preview {
+        MainView()
+    }
--- FetchDemo/FetchDemo/MealDetailView.swift
+++ FetchDemo/FetchDemo/MealDetailView.swift
@@ -14,85 +14,108 @@ struct MealDetailView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
-                if let mealDetails = mealDetail {
-                    AsyncImage(url: URL(string: mealDetails.strMealThumb)) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(height: 250)
-                            .clipped()
-                            .cornerRadius(15)
-                            .shadow(radius: 10)
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    
-                    
-                    Text(mealDetails.strMeal)
-                        .font(.largeTitle)
-                        .bold()
-                        .padding(.top)
-                        .multilineTextAlignment(.center)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                    
-                    VStack(spacing: 8) {
-                        Text("Ingredients")
-                            .font(.title2)
-                            .bold()
-                            .padding(.bottom, 5)
-                        
-                        
-                        ForEach(mealDetails.ingredients, id: \.self) { ingredient in
-                            
-                            Text("• \(ingredient)")
-                                .font(.body)
-                                .padding(.leading, 8)
-                                .padding(.bottom, 2)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.purple.opacity(0.5)]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    if let mealDetails = mealDetail {
+                        AsyncImage(url: URL(string: mealDetails.strMealThumb)) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 250)
+                                .clipped()
+                                .cornerRadius(15)
+                                .shadow(radius: 10)
+                                .overlay(
+                                    LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.6), Color.clear]), startPoint: .bottom, endPoint: .center)
+                                )
+                                .cornerRadius(15)
+                        } placeholder: {
+                            ProgressView()
                         }
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                    
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Instructions")
-                            .font(.title2)
-                            .bold()
-                            .padding(.bottom, 5)
                         
-                        ForEach(formatInstructions(mealDetails.strInstructions), id: \.self) { instruction in
+                        
+                        Text(mealDetails.strMeal)
+                            .font(.largeTitle)
+                            .foregroundColor(.primary)
+                            .bold()
+                            .padding(.top)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        
+                        VStack(spacing: 8) {
+                            Text("Ingredients")
+                                .font(.title2)
+                                .bold()
+                                .padding(.bottom, 5)
                             
-                            Text("• \(instruction)")
-                                .font(.body)
-                                .padding(.leading, 8)
-                                .padding(.bottom, 2)
-                        }
-                    }
-                    .padding()
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                    
-                } else {
-                    Text("Loading...")
-                        .task {
-                            do {
-                                mealDetail = try await apiService.fetchMealDetails(id: mealId)
-                            } catch {
-                                print("Error fetching meal details: \(error.localizedDescription)")
+                            
+                            ForEach(mealDetails.ingredients, id: \.self) { ingredient in
+                                
+                                Text("• \(ingredient)")
+                                    .font(.body)
+                                    .padding(.leading, 8)
+                                    .padding(.bottom, 2)
+                                    .foregroundColor(.secondary)
                             }
                         }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Instructions")
+                                .font(.title2)
+                                .bold()
+                                .padding(.bottom, 5)
+                            
+                            ForEach(formatInstructions(mealDetails.strInstructions), id: \.self) { instruction in
+                                
+                                Text("• \(instruction)")
+                                    .font(.body)
+                                    .padding(.leading, 8)
+                                    .padding(.bottom, 2)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        
+                    } else {
+                        VStack {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .scaleEffect(1.5)
+                                .padding(.top, 50)
+                            
+                            Text("Loading Details...")
+                                .font(.body)
+                                .foregroundColor(.secondary)
+                                .padding(.top, 10)
+                            
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    }
                 }
+                .padding([.leading, .trailing])
             }
-            .padding([.leading, .trailing])
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: customBackButton)
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: customBackButton)
-        .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
+        .task {
+            do {
+                mealDetail = try await apiService.fetchMealDetails(id: mealId)
+            } catch {
+                print("Error fetching meal details: \(error.localizedDescription)")
+            }
+        }
     }
     
     private func formatInstructions(_ instructions: String) -> [String] {
@@ -107,9 +130,9 @@ struct MealDetailView: View {
         }) {
             HStack{
                 Image(systemName: "chevron.backward.circle")
-                    .foregroundColor(.purple)
+                    .foregroundColor(.black)
                 Text("Back")
-                    .foregroundColor(.purple)
+                    .foregroundColor(.black)
             }
         }
     }
